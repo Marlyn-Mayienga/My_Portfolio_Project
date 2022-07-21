84bca6a73e49f7eb5f77aa5124f3b5161f1af412
Marlyn-Mayienga <marlymayienga@gmail.com>
2022-07-21 23:14:19 +0300

    feat:added data persistence in form submit

--- main.js
+++ main.js
@@ -18,22 +18,14 @@ menu.addEventListener('click', toggleMenu);
 hamburger.addEventListener('click', toggleMenu);
 
 // Form-validation//
+const form = document.getElementById('form');
 
 // Check if String has Uppercase
 function checkUppercase(str) {
-  for (let i = 0; i < str.length; i += 1) {
-    if (
-      str.charAt(i) !== str.charAt(i).toUpperCase()
-      && !str.charAt(i).match(/[a-z]/i)
-    ) {
-      return false;
-    }
-  }
-  return true;
+  return str !== str.toLowerCase();
 }
 
 function handleSubmit(e) {
-  e.preventDefault();
   const username = document.getElementById('username');
   const email = document.getElementById('email');
   const errorMessage = document.getElementById('error');
@@ -42,47 +34,51 @@ function handleSubmit(e) {
   if (username.value === '') {
     errorMessage.textContent = 'Name cannot be blank';
     errorMessage.classList.add('show');
+    e.preventDefault();
     return false;
   }
-  errorMessage.classList.remove('show');
 
   if (username.value.length < 3 || username.value.length > 20) {
     errorMessage.textContent = 'Name must have between 3 to 20 characters';
     errorMessage.classList.add('show');
+    e.preventDefault();
     return false;
   }
-  errorMessage.classList.remove('show');
 
   if (checkUppercase(email.value)) {
     errorMessage.textContent = 'Most enter a valid email in lower case';
     errorMessage.classList.add('show');
+    e.preventDefault();
     return false;
   }
-  errorMessage.classList.remove('show');
 
   if (message.value.length > 1000 || message.value === '') {
     errorMessage.textContent = 'Most write a message of less than 1000 characters';
     errorMessage.classList.add('show');
-  } else {
-    errorMessage.classList.remove('show');
+    e.preventDefault();
+    return false;
   }
+  errorMessage.classList.remove('show');
+
   const obj = {
     fullname: username.value,
-    EmailAdress: email.value,
+    emailAddress: email.value,
     message: message.value,
   };
 
   localStorage.setItem('DATA', JSON.stringify(obj));
   return true;
 }
-handleSubmit();
+
+form.addEventListener('submit', handleSubmit);
+
 const getData = localStorage.getItem('DATA');
 const getDataValue = JSON.parse(getData);
 
 window.addEventListener('load', () => {
   if (localStorage.getItem('DATA')) {
     document.querySelector('#username').value = getDataValue.fullname;
-    document.querySelector('#email').value = getDataValue.EmailAdress;
+    document.querySelector('#email').value = getDataValue.emailAddress;
     document.querySelector('#msg').value = getDataValue.message;
   }
 });
